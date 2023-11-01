16c6cdc01d15713ba522035293fb767304ee3484
Biruk Tafesse <98220455+biruk-tafese@users.noreply.github.com>
2023-11-02 00:07:51 +0300

    simple portfolio

---
+++ Sidebar2/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggler = document.querySelector(".btn");
+
+    toggler.addEventListener("click", function () {
+        document.querySelector("#sidebar").classList.toggle("collapsed");
+    });
+});
